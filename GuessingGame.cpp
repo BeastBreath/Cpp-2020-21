5f45fbc23566c4c9163cfb06627ba1ce6cbc209b
Nividh S <Rawrocky5280@gmail.com>
2020-09-16 14:29:17 -0700

    Play again Function

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -2,17 +2,46 @@
 
 using namespace std;
 
+bool checkIfPlayAgain()
+{
+  bool answer = false;
+  char ch = 'a';
+  while(!answer)
+  {
+    cout << "Do you want to play again? y for yes, n for no" << endl;
+    cin >> ch;
+    if (ch == 'y')
+    {
+      return true;
+      answer = true;
+    }
+    else if (ch == 'n')
+    {
+      return false;
+      answer = true;
+    }
+  }
+  return false;
+}
+
 int main()
 {
-  cout << "Try to guess a random number\n";
   int randomNumber = 5;
   int input = -1;
-  while(input != randomNumber)
+  bool play = true;
+  while(play)
   {
-    cin >> input;
+    cout << "Try to guess the number!" << endl;
+    while(input != randomNumber)
+    {
+      cin >> input;
+    }
+    if(checkIfPlayAgain() == false)
+    {
+      play = false;
+    }
+    input = -1;
   }
   return 0;
 }
-
-
 
