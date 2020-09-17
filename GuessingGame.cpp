388fa360d9d4bac9de9cac1b7542d805d7a3c526
Nividh S <Rawrocky5280@gmail.com>
2020-09-17 15:28:54 -0700

    Finished Commenting

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -2,20 +2,22 @@
 
 using namespace std;
 
+//Method for asking if player wants to play again
 bool checkIfPlayAgain()
 {
-  bool answer = false;
-  char ch = 'a';
+  bool answer = false;//Varialbe for if answer is valid
+  char ch = 'a';//Char for the answer
+  //While loop for if the answer isn't valid
   while(!answer)
   {
     cout << "Do you want to play again? y for yes, n for no" << endl;
     cin >> ch;
-    if (ch == 'y')
+    if (ch == 'y')//If answer is yes
     {
       return true;
       answer = true;
     }
-    else if (ch == 'n')
+    else if (ch == 'n')//If answer is no
     {
       return false;
       answer = true;
@@ -24,37 +26,38 @@ bool checkIfPlayAgain()
   return false;
 }
 
+//main method
 int main()
 {
   srand(time(NULL));
-  int randomNumber = rand()%101;
-  int input = -1;
-  bool play = true;
+  int randomNumber = rand()%101;//random number
+  int input = -1;//input number
+  bool play = true;//boolean for if the game is being played
   while(play)
   {
     cout << "Try to guess the number!" << endl;
-    while(input != randomNumber)
+    while(input != randomNumber)//While the guess isn't correct
     {
       cin >> input;
-      if(input > randomNumber)
+      if(input > randomNumber)//If the guess is too high
       {
 	cout << "Your guess is too high" << endl;
       }
-      else if(input < randomNumber)
+      else if(input < randomNumber)//If the guess is too low
       {
 	cout << "Your guess is too low" << endl;
       }
-      else
+      else//If the guess is correct
       {
       cout << "You are Correct!" << endl;
       }
     }
-    if(checkIfPlayAgain() == false)
+    if(checkIfPlayAgain() == false)//Check if player wants to play again
     {
       play = false;
     }
-    input = -1;
+    input = -1;//Reset the input
+    randomNumber = rand()%101;//Reset random number
   }
   return 0;
 }
-
