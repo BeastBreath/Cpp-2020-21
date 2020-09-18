b197cee8629c4a7a750fb87cc59575961645bf48
Nividh S <Rawrocky5280@gmail.com>
2020-09-18 13:38:13 -0700

    Finished Program
    Finished debugging and fixing everything

--- Palindrome/Palindrome.cpp
+++ Palindrome/Palindrome.cpp
@@ -1,13 +1,25 @@
+/*
+Program Created by Nividh Singh on 9/17/20
+Takes in an input and outputs if it is or if it isn't a palindrome
+The definition of a palindrome for this project is a string of whose alphanumeric charectors read the same forward and backward, and is not case sensative
+*/
+
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
-bool checkIfPalindrome(char input[80], int half, int length)
+//Method for checking if the input is a Palindrome
+bool checkIfPalindrome(char input[80])
 {
-  cout << "A" << input << endl;
+  
+  int length = strlen(input);
+  int half = length/2;
+  //for loop that goes from the start to the midde
   for(int i = 0; i < half; i++) {
+    //If statement comparing the value in the first half to that in the second half
     if (input[i] != input[length - 1 - i]) {
+      cout << i << endl;
       return false;
     }
   }
@@ -15,21 +27,39 @@ bool checkIfPalindrome(char input[80], int half, int length)
   
 }
 
+//main method
 int main()
 {
-
+  cout << "Please input something" << endl;
   char input[80];
-  cout << "Enter your input\n";
   cin.get(input, 80);
   cin.get();
+  char alphaNumInput[80];
+  int count = 0;
   int length = strlen(input);
-  int half = length/2;
-  if(length%2 == 1) {
-    half++;
+  //Takes out spaces and punctuation and makes everything uppercase
+  for(int i = 0; i < length; i++) {
+    if(input[i] < 123 && input[i] > 96) {
+      //Lower Case
+      alphaNumInput[count] = input[i] - 32;
+      count++;
+    }
+    else if(input[i] < 91 && input[i] > 64) {
+      //Upper Case
+      alphaNumInput[count] = input[i];
+      count++;
+    }
+    else if(input[i] < 58 && input[i] > 47) {
+      //Number
+      alphaNumInput[count] = input[i];
+      count++;
+    }
   }
-  bool isPalindrome = checkIfPalindrome(input, half, length);
+  alphaNumInput[count] = '\0';  
+  bool isPalindrome = checkIfPalindrome(alphaNumInput);
+  //Prints is palindrome if the answer is a palindrome. Otherwise prints it is not a palindrome.
   if(isPalindrome) {
-    cout << "It is a palindrome" << endl;
+    cout << "This is a palindrome" << endl;
   }
   else {
     cout << "It is not a palindrome" << endl;
