93926428f27316017621c9f7e00733da441d3227
Nividh S <Rawrocky5280@gmail.com>
2020-09-19 02:05:54 -0700

    Finished everything besides one bug

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,7 +1,13 @@
+/*
+Made by Nividh Singh on 9/19/20
+This program is a two player tic tac toe game
+*/
 #include <iostream>
 #include <cstring>
 
 using namespace std;
+
+//Constants used in the program
 const int OPEN = 0;
 const int X = 1;
 const int O = 2;
@@ -11,28 +17,35 @@ bool checkWin(int board[][3], int turn);
 bool checkDraw(int board[][3]);
 void drawBoard(int board[][3]);
 
+//Main Method
 int main()
 {
   int board[3][3];
   char input[2];
+  //Loop that sets everything to open
   for(int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
       board[i][j] = OPEN;
+      cout << board[i][j];
     }
+    cout << endl;
   }
   bool playing = true;
   int turn = X;
   cout << "Start" << endl;
   while(playing) {
+    cout << "1: " << board[0][0] << endl;
     cin >> input;
+    cout << "2: " << board[0][0] << endl;
     if(checkValidAndPlace(board, input, turn)) {
-      cout << "Worked!" << endl;
+
+      board[input[0] - 'a'][input[1] - '1'] = turn;
       if(turn == X) {
 	if(checkWin(board, turn)) {
 	  cout << "X won the game" << endl;
 	  playing = false;
 	}
-	turn = 0;
+	turn = O;
       }
       else {
 	if(checkWin(board, turn)) {
@@ -51,48 +64,37 @@ int main()
   }
 }
 
-
+//Method for drawing board
 void drawBoard(int board[][3]) {
+  //Goes through each row
   for(int i = 0; i < 3; i++) {
-    for(int j = 0; j < 3; j++) {
-      if(board[i][j] == X){
-	cout << "X\t";
-      }
-      else if(board[i][j] == O) {
-	cout << "O\t";
-      }
-      else{
-	cout << "\t";
-      }
+    //Goes through each column
+    for (int j = 0; j < 3; j++) {
+      cout << board[i][j];
     }
     cout << endl;
   }
+
 }
 
-bool checkValidAndPlace(int board[][3], char* position, int turn) {
-  cout << "This is the board spot: " << *board[1] << endl;
-  cout << "OPEN: " << OPEN << endl;
+//Boolean for checking if the input is valid
+bool checkValidAndPlace(int board[][3], char position[2], int turn) {
   //Check if the input is on the board
   if(!((position[0] == 'a' || position[0] == 'b' || position[0] == 'c') && (position[1] == '1' || position[1] == '2' || position[1] == '3'))) {
-    cout << "first" << endl;
     return false;
   }
 
-  //Check if the input is valid
-  cout << "Checkin 1\n";
+  //Check if the input is not taken by another piece
   int row = position[0] - 'a';
   int column = position[1] - '1';
-  cout << row << "    " << column << endl;
-  cout << board[0][0] << endl;
-  cout << board[row][column] << endl;
+  cout << "turn: " << turn << endl;
   if(board[row][column] == OPEN) {
-    cout << "Check in 2\n";
-    board[row][column] == turn;
     return true;
   }
   return false;
 }
 
+//Check if a player has won
 bool checkWin(int board[][3], int turn) {
   //Loop to go through three rows/columns
   for(int i = 0; i < 3; i++){
@@ -118,11 +120,14 @@ bool checkWin(int board[][3], int turn) {
   return false;
 }
 
+//Check if the game is a draw
 bool checkDraw(int board[][3]) {
+  //Goes through each row
   for(int i = 0; i < 3; i++) {
+    //Goes through each column
     for (int j = 0; j < 3; j++) {
       if(board[i][j] == OPEN) {
-	return false;
+	return false;//if anything is open, it returns false
       }
     }
   }
